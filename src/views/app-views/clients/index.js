4336fb3b8da551a02d44f772f523d04cd3e0f40b
Pavel Smirnov <smrnvpvl@gmail.com>
2022-10-17 20:59:31 +0300

    feat: starter Clients setup

--- src/views/app-views/clients/index.js
+++ src/views/app-views/clients/index.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Clients = () => {
-  return <div>Clients component works!</div>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(function () {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((res) => {
+        setUsers(res);
+        console.log(res);
+      });
+  }, []);
+
+  return (
+    <div>
+      <ul className="">
+        {users?.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Clients;
